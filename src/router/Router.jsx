3d814abe3d67774f044d092c45c86033f9f06e00
L2HYUNN <dhl9810@naver.com>
2022-09-07 22:21:28 +0900

    amend router setting

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,26 +1,28 @@
 import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import Analysis from "../pages/Analysis";
 import ChatDetail from "../pages/ChatDetail";
 import CheckReservation from "../pages/CheckReservation";
+import Child from "../pages/Child";
 import Consulting from "../pages/Consulting";
 import Doctor from "../pages/Doctor";
 import Home from "../pages/Home";
 import Join from "../pages/Join";
 import Login from "../pages/Login";
-import Main from "../pages/Main";
-import MyPage from "../pages/MyPage";
+import Logout from "../pages/Logout";
 import Reserve from "../pages/Reserve";
 
 function Router() {
   return (
     <Routers>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/join" element={<Join />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/child" element={<Child />} />
+        <Route path="/analysis" element={<Analysis />} />
         <Route path="/chat" element={<ChatDetail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/:id/reserve" element={<CheckReservation />} />
+        <Route path="/reservation" element={<CheckReservation />} />
         <Route path="/consulting" element={<Consulting />} />
         <Route path="/consulting/:id" element={<Doctor />} />
         <Route path="/consulting/:id/reserve" element={<Reserve />} />
