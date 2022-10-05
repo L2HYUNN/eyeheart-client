e835cd67c5b304b62a4560eb23607572dd259c7e
L2HYUNN <donghyuni98@gmail.com>
2022-10-05 17:12:28 +0900

    amend analysis pages

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,25 @@
+import moment from "moment";
+import { useEffect } from "react";
+import { useState } from "react";
 import ApexChart from "react-apexcharts";
+import { useQuery } from "react-query";
 import styled from "styled-components";
+import { getAnalysisHeart } from "../api/api";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 72rem;
-  height: 50rem;
+  height: 51rem;
   grid-area: weather;
+  @media ${({ theme }) => theme.size.small} {
+    min-width: max-content;
+    width: 100vw;
+    padding-left: 0rem;
+    padding-top: 3rem;
+    height: 100%;
+  }
 `;
 const Contents = styled.div`
   display: flex;
@@ -16,15 +28,57 @@ const Contents = styled.div`
   width: 67rem;
   height: 45rem;
   background-color: white;
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
   border-radius: 2rem;
+  @media ${({ theme }) => theme.size.small} {
+    flex-direction: column;
+    width: 90%;
+    height: 100%;
+  }
 `;
 const Graph = styled.div`
   width: 67rem;
+  @media ${({ theme }) => theme.size.small} {
+    width: 90%;
+    /* font-size: 1.6rem; */
+  }
 `;
 
+const day = moment().format("YYYYMMDD");
+
 function Weather() {
+  const [date, setDate] = useState([]);
+  const [score, setScore] = useState([]);
+  const { isLoading, data } = useQuery("analysisHeart", () =>
+    getAnalysisHeart(day)
+  );
+
+  useEffect(() => {
+    if (!isLoading) {
+      setDate(
+        data?.data?.statistics
+          .map(({ date: newDate }) => {
+            const day = newDate.slice(4).split("");
+            day.splice(2, 0, "/");
+            return day.join("");
+          })
+          .reverse()
+      );
+      setScore(
+        data?.data?.statistics
+          .map(
+            ({
+              chart: {
+                emotion: { emotion_score },
+              },
+            }) => emotion_score
+          )
+          .reverse()
+      );
+    }
+  }, [isLoading, data]);
+
   return (
     <Container>
       <Contents>
@@ -34,7 +88,7 @@ function Weather() {
             series={[
               {
                 name: "마음지수",
-                data: [45, 67, 84, 66, 87, 76, 95],
+                data: score,
               },
             ]}
             options={{
@@ -48,10 +102,10 @@ function Weather() {
               },
               colors: "#FFC6AE",
               xaxis: {
-                categories: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
+                categories: date,
                 labels: {
                   style: {
-                    fontSize: "1.4rem",
+                    fontSize: "1.6rem",
                     fontWeight: 600,
                   },
                 },
