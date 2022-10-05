cae637500d13af9cf7cea5a6b114650edc6870aa
L2HYUNN <donghyuni98@gmail.com>
2022-10-05 17:15:34 +0900

    amend chatDetail pages

--- src/pages/ChatDetail.jsx
+++ src/pages/ChatDetail.jsx
@@ -3,6 +3,7 @@ import child from "../assets/child.jpeg";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Calendar from "react-calendar";
+import ApexChart from "react-apexcharts";
 import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
 import moment from "moment";
@@ -10,7 +11,12 @@ import { socket } from "../App";
 import { useEffect } from "react";
 import { useRef } from "react";
 import { useQuery } from "react-query";
-import { getChat } from "../api/api";
+import {
+  getChat,
+  getChatBadWord,
+  getChatScenario,
+  getChatTopic,
+} from "../api/api";
 
 const Main = styled.main`
   display: flex;
@@ -36,11 +42,77 @@ const Wrapper = styled.div`
     rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
   padding: 2rem;
   border-radius: 2rem;
+  @media ${({ theme }) => theme.size.small} {
+    width: 90%;
+    height: 100%;
+    padding: 5rem 0;
+  }
 `;
 const CalendarContents = styled.div`
   width: 40rem;
   height: 93.5;
+  margin-top: 5rem;
+  @media ${({ theme }) => theme.size.small} {
+    width: 90%;
+  }
 `;
+const CalendarScenario = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const CalendarScenarioTitle = styled.p`
+  font-size: 2.6rem;
+  font-weight: 500;
+  margin-bottom: 3rem;
+  @media ${({ theme }) => theme.size.small} {
+    font-size: 2rem;
+  }
+`;
+const CalendarScenarioTexts = styled.ul`
+  width: 100%;
+  text-align: center;
+`;
+const CalendarScenarioText = styled.li`
+  font-size: 1.8rem;
+  margin-bottom: 1rem;
+  @media ${({ theme }) => theme.size.small} {
+    font-size: 1.6rem;
+  }
+`;
+const CalendarGraph = styled.div`
+  width: 100%;
+  margin-top: 5rem;
+`;
+
+const CalendarBadWord = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 3rem;
+`;
+const CalendarBadWordTitle = styled.p`
+  font-size: 2.6rem;
+  font-weight: 500;
+  margin-bottom: 3rem;
+  @media ${({ theme }) => theme.size.small} {
+    font-size: 2rem;
+  }
+`;
+const CalendarBadWordTexts = styled.ul`
+  width: 100%;
+  text-align: center;
+`;
+const CalendarBadWordText = styled.li`
+  font-size: 1.8rem;
+  margin-bottom: 1rem;
+  @media ${({ theme }) => theme.size.small} {
+    font-size: 1.6rem;
+  }
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -53,8 +125,8 @@ const Container = styled.div`
     min-width: max-content;
     min-height: max-content;
     width: 100%;
-    width: 36.5rem;
     height: 100%;
+    margin-bottom: 3rem;
   }
 `;
 const Contents = styled.div`
@@ -62,7 +134,7 @@ const Contents = styled.div`
   flex-direction: column;
   align-items: center;
   width: 90rem;
-  height: 100rem;
+  height: 120rem;
   background-color: ${({ theme }) => theme.colors.pink};
   box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
     rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
@@ -73,7 +145,7 @@ const Contents = styled.div`
   @media ${({ theme }) => theme.size.small} {
     max-width: 100vw;
     max-height: fit-content;
-    width: 100vw;
+    width: 100%;
     height: 100%;
     padding: 1.2rem;
     border-top-left-radius: 0rem;
@@ -105,7 +177,6 @@ const ChatBox = styled.div`
   width: 100%;
   @media ${({ theme }) => theme.size.small} {
     max-height: fit-content;
-    height: 100%;
   }
 `;
 const Chatlog = styled.ul`
@@ -113,10 +184,10 @@ const Chatlog = styled.ul`
   flex-direction: column;
   /* justify-content: space-between; */
   overflow-y: scroll;
-  height: 90rem;
-  /* &::-webkit-scrollbar {
+  height: 95rem;
+  &::-webkit-scrollbar {
     display: none;
-  } */
+  }
   -ms-overflow-style: none;
   scrollbar-width: none;
   @media ${({ theme }) => theme.size.small} {
@@ -204,10 +275,12 @@ const UserChatTime = styled.div`
 `;
 const ChatForm = styled.form`
   width: 90rem;
-  height: 20rem;
+  height: 10rem;
   border: none;
   box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
     rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
   @media ${({ theme }) => theme.size.small} {
     max-width: 100vw;
     width: 100%;
@@ -216,11 +289,13 @@ const ChatForm = styled.form`
 `;
 const ChatInput = styled.input`
   width: 70rem;
-  height: 20rem;
+  height: 10rem;
   font-size: 2rem;
   border: none;
   outline: none;
   padding: 2rem;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
   @media ${({ theme }) => theme.size.small} {
     font-size: 1.6rem;
     min-width: max-content;
@@ -230,7 +305,7 @@ const ChatInput = styled.input`
 `;
 const ChatButton = styled.button`
   width: 17rem;
-  height: 15rem;
+  height: 8rem;
   background-color: #a5e4fe;
   box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
     rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
@@ -245,125 +320,70 @@ const ChatButton = styled.button`
   }
 `;
 
-const DummyChat = [
-  {
-    id: 1,
-    name: "민영이",
-    chat: "도담아",
-    time: "오후 4:48",
-    role: "child",
-  },
-  {
-    id: 2,
-    chat: "네?",
-    time: "오후 4:48",
-    role: "user",
-  },
-  {
-    id: 3,
-    name: "민영이",
-    chat: "오늘 학교에서 재밌는 일이 있었는데 한 번 들어볼래?",
-    time: "오후 4:48",
-    role: "child",
-  },
-  {
-    id: 4,
-    chat: "네, 무슨 일인지 정말 궁금해요!",
-    time: "오후 4:48",
-    role: "user",
-  },
-  {
-    id: 5,
-    name: "민영이",
-    chat: "그럼 한 번 들어봐! 오늘 내가 색종이를 하나 가져왔는데 친구들이 재밌게 놀다가 잃어버렸다는 거야. 그런데 알고보니까 그 색종이를 누가 내 가방에 넣어놨던 거 였어!!",
-    time: "오후 4:49",
-    role: "child",
-  },
-  {
-    id: 6,
-    chat: "그런 일이 있었군요. 모두가 웃을 정도로 재밌는 일이었겠네요.",
-    time: "오후 4:49",
-    role: "user",
-  },
-  {
-    id: 7,
-    name: "민영이",
-    chat: "그렇지, 반 내에 모두가 한참동안 웃었어. 우리가 그 색종이 찾으려고 엄청 고생했거든!",
-    time: "오후 4:49",
-    role: "child",
-  },
-  {
-    id: 8,
-    chat: "모두가 웃을정도였으면 정말로 재밌었던 일이었겠어요! ",
-    time: "오후 4:50",
-    role: "user",
-  },
-  {
-    id: 9,
-    name: "민영이",
-    chat: "도담아",
-    time: "오후 5:12",
-    role: "child",
-  },
-  {
-    id: 10,
-    chat: "네?",
-    time: "오후 5:12",
-    role: "user",
-  },
-  {
-    id: 11,
-    name: "민영이",
-    chat: "내 친구 지수랑 사이가 멀어질까봐 걱정 돼.",
-    time: "오후 5:12",
-    role: "child",
-  },
-  {
-    id: 12,
-    chat: "무슨 일이 있었는지 자세히 알려줄 수 있나요?",
-    time: "오후 5:13",
-    role: "user",
-  },
-  {
-    id: 13,
-    name: "민영이",
-    chat: "체육시간 때 지수랑 짝궁을 하기로 했는데 현수가 같이 하자고 졸라대서 어쩔 수 없이 현수랑 하게 되었거든, 그래서 지수가 많이 서운해 한 거 같아.",
-    time: "오후 5:13",
-    role: "child",
-  },
-  {
-    id: 14,
-    chat: "그런 일이 있었군요. 풍부한 상상력은 때로는 많은 걱정을 낳기도 한대요. 내일 친구랑 직접 얘기해보는 건 어떨까요? 분명 그 친구도 이해해줄 거에요.",
-    time: "오후 5:13",
-    role: "user",
-  },
-  {
-    id: 15,
-    name: "민영이",
-    chat: "고마워. 한 번 지수한테 얘기 해볼게.",
-    time: "오후 5:13",
-    role: "child",
-  },
-  {
-    id: 16,
-    chat: "그래요. 저도 응원할게요!",
-    time: "오후 5:14",
-    role: "user",
-  },
-];
-
 function ChatDetail() {
   const [date, setDate] = useState(new Date());
   const [chat, setChat] = useState([]);
   const [message, setMessage] = useState("");
+  const [scenario, setScenario] = useState([]);
+  const [badWord, setBadWord] = useState({ 슬픔: 0 });
+  const [badSentence, setBadSentence] = useState({});
+  const [topic, setTopic] = useState([]);
   const scrollRef = useRef(null);
 
-  const scrollToBottom = () => {
-    console.log(scrollRef.current);
-  };
-  const { isLoading, data } = useQuery("chatList", () =>
-    getChat(moment(date).format("YYMMDD"))
+  const chatList = useQuery("chatList", () =>
+    getChat(moment(date).format("YYYYMMDD"))
   );
+  const chatScenario = useQuery("chatScenario", () =>
+    getChatScenario(moment(date).format("YYYYMMDD"))
+  );
+  const chatBadWord = useQuery("chatBadWord", () =>
+    getChatBadWord(moment(date).format("YYYYMMDD"))
+  );
+  const chatTopic = useQuery("chatTopic", () =>
+    getChatTopic(moment(date).format("YYYYMMDD"))
+  );
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat]);
+
+  useEffect(() => {
+    chatList.refetch();
+    chatScenario.refetch();
+    chatBadWord.refetch();
+    chatTopic.refetch();
+  }, [date]);
+
+  useEffect(() => {
+    if (!chatList.isLoading) {
+      setChat(chatList.data.data.chats);
+    }
+  }, [chatList.data, chatList.isLoading]);
+
+  useEffect(() => {
+    if (!chatScenario.isLoading) setScenario(chatScenario.data.data.message);
+  }, [chatScenario.data, chatScenario.isLoading, date]);
+
+  useEffect(() => {
+    if (!chatBadWord.isLoading) {
+      setBadWord(chatBadWord.data.data.statistics[0]?.chart.badness.bad_words);
+      setBadSentence(
+        chatBadWord.data.data.statistics[0]?.chart.badness.bad_sentences
+      );
+    }
+  }, [chatBadWord.data, chatBadWord.isLoading, date]);
+
+  useEffect(() => {
+    if (!chatTopic.isLoading) {
+      setTopic(
+        Object.entries(chatTopic.data.data.summary.situation.topic).slice(0, 5)
+      );
+    }
+  }, [chatTopic.data, chatTopic.isLoading, date]);
+
+  const scrollToBottom = () => {
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  };
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -374,27 +394,121 @@ function ChatDetail() {
 
     socket.emit("SEND_MESSAGE", { type: "SUPERVISOR", message });
 
-    setChat((prev) => [...prev, { role: "doll", message, day, time }]);
+    setChat((prev) => [
+      ...prev,
+      { direction: "BOT", utterance: message, day, time },
+    ]);
     setMessage("");
   };
 
   useEffect(() => {
     socket.on("RECEIVE_MESSAGE", ({ response: message, day, time }) => {
-      setChat((prev) => [...prev, { role: "child", message, day, time }]);
+      setChat((prev) => [
+        ...prev,
+        { direction: "USER", utterance: message, day, time },
+      ]);
     });
   }, [setChat]);
-
-  useEffect(() => {
-    if (!isLoading) console.log(data);
-  }, [data, isLoading]);
 
   return (
     <>
       <Header />
       <Main>
         <Wrapper>
-          <Calendar onChange={setDate} value={date} />
-          <CalendarContents></CalendarContents>
+          <Calendar
+            onChange={(value) => {
+              setDate(value);
+            }}
+            value={date}
+          />
+          <CalendarContents>
+            <CalendarScenario>
+              <CalendarScenarioTitle>
+                대화중 아이와 나눈 고민
+              </CalendarScenarioTitle>
+              <CalendarScenarioTexts>
+                {scenario.map((info, index) => {
+                  return (
+                    <CalendarScenarioText key={index}>
+                      {info?.name}에 대한 고민이 {info?.number}건
+                    </CalendarScenarioText>
+                  );
+                })}
+              </CalendarScenarioTexts>
+            </CalendarScenario>
+            <CalendarGraph>
+              <ApexChart
+                type="bar"
+                series={[
+                  {
+                    name: "갯수",
+                    data: topic.map((info) => info[1].total),
+                  },
+                ]}
+                options={{
+                  chart: {
+                    height: 100,
+                    width: 100,
+                    toolbar: {
+                      show: false,
+                    },
+                    background: "white",
+                  },
+                  colors: "#FFC6AE",
+                  xaxis: {
+                    categories: topic.map((info) => info[0]),
+                    labels: {
+                      style: {
+                        fontSize: "1.4rem",
+                        fontWeight: 600,
+                      },
+                    },
+                    axisTicks: {
+                      show: false,
+                    },
+                    axisBorder: {
+                      show: false,
+                    },
+                    position: "top",
+                  },
+                  yaxis: {
+                    max: 10,
+                    show: false,
+                  },
+                  grid: {
+                    show: false,
+                  },
+                  dataLabels: {
+                    enabled: false,
+                  },
+                }}
+              ></ApexChart>
+            </CalendarGraph>
+            <CalendarBadWord>
+              <CalendarBadWordTitle>
+                아이가 사용한 위험 단어 리스트
+              </CalendarBadWordTitle>
+              <CalendarBadWordTexts>
+                {badWord
+                  ? Object.keys(badWord).map((word, index) => (
+                      <CalendarBadWordText key={index}>
+                        {word}
+                      </CalendarBadWordText>
+                    ))
+                  : null}
+              </CalendarBadWordTexts>
+            </CalendarBadWord>
+            <CalendarBadWord>
+              <CalendarBadWordTitle>
+                아이가 사용한 위험 문장 리스트
+              </CalendarBadWordTitle>
+              <CalendarBadWordTexts>
+                {badSentence?.sentences?.map((word, index) => (
+                  <CalendarBadWordText key={index}>{word}</CalendarBadWordText>
+                ))}
+              </CalendarBadWordTexts>
+            </CalendarBadWord>
+          </CalendarContents>
         </Wrapper>
         <Container>
           <Contents>
@@ -403,40 +517,15 @@ function ChatDetail() {
             </DateBox>
             <ChatBox>
               <Chatlog ref={scrollRef}>
-                {DummyChat.map((user) => {
-                  if (user.role === "child") {
+                {chat?.map((info, index) => {
+                  if (info.direction === "USER") {
                     return (
-                      <ChildContents key={user.id}>
-                        <ChildImg src={child}></ChildImg>
-                        <ChildInfo>
-                          <ChildName>{user.name}</ChildName>
-                          <ChildChats>
-                            <ChildChat>{user.chat}</ChildChat>
-                            <ChildChatTime>{user.time}</ChildChatTime>
-                          </ChildChats>
-                        </ChildInfo>
-                      </ChildContents>
-                    );
-                  } else {
-                    return (
-                      <UserContents key={user.id}>
-                        <UserChats>
-                          <UserChatTime>{user.time}</UserChatTime>
-                          <UserChat>{user.chat}</UserChat>
-                        </UserChats>
-                      </UserContents>
-                    );
-                  }
-                })}
-                {chat.map((info) => {
-                  if (info.role === "child") {
-                    return (
-                      <ChildContents key={info.time}>
+                      <ChildContents key={index}>
                         <ChildImg src={child}></ChildImg>
                         <ChildInfo>
                           <ChildName>{info.name || "민영이"}</ChildName>
                           <ChildChats>
-                            <ChildChat>{info.message}</ChildChat>
+                            <ChildChat>{info.utterance}</ChildChat>
                             <ChildChatTime>{info.time}</ChildChatTime>
                           </ChildChats>
                         </ChildInfo>
@@ -444,10 +533,10 @@ function ChatDetail() {
                     );
                   } else {
                     return (
-                      <UserContents key={info.time}>
+                      <UserContents key={index}>
                         <UserChats>
                           <UserChatTime>{info.time}</UserChatTime>
-                          <UserChat>{info.message}</UserChat>
+                          <UserChat>{info.utterance}</UserChat>
                         </UserChats>
                       </UserContents>
                     );
