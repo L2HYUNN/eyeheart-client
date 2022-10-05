34b04be49e52c482243e693b040a3fd506e5e2e8
L2HYUNN <donghyuni98@gmail.com>
2022-10-05 17:00:52 +0900

    add new apis

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { get } from "react-hook-form";
 
 const BASE_URL = "http://125.6.39.158:5000";
 
@@ -45,4 +46,51 @@ export const postConsultingReservation = async (reservation) => {
 
 export const getUserReservation = async (id) => {
   return await axios.get(`${BASE_URL}/reservations/user/${id}`);
+};
+
+// {{base_url}}/chats/day/20220910
+
+export const getChat = async (date) => {
+  return await axios.get(`${BASE_URL}/chats/day/${date}`);
+};
+
+// {{base_url}}/stat/recent/scenario/20221002
+
+export const getChatScenario = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/recent/scenario/${date}`);
+};
+
+//{{base_url}}/stat/bad/day/20220918
+
+export const getChatBadWord = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/bad/day/${date}`);
+};
+
+// {{base_url}}/stat/topic/latest/20221004/before/7
+
+export const getChatTopic = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/topic/latest/${date}/before/7`);
+};
+
+// {{base_url}}/stat/relation/latest/20220919/before/7
+
+export const getAnalysisRelation = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/relation/latest/${date}/before/7`);
+};
+
+// {{base_url}}/stat/recent/words/today/20220920
+
+export const getAnalysisWord = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/recent/words/today/${date}`);
+};
+
+// {{base_url}}/stat/emotion/latest/20220922/before/7
+// {{base_url}}/stat/recent/emotions/20221004
+
+export const getAnalysisHeart = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/emotion/latest/${date}/before/7`);
+};
+
+export const getAnalysisEmotion = async (date) => {
+  return await axios.get(`${BASE_URL}/stat/recent/emotions/${date}`);
 };
